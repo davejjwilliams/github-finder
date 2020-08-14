874f4fa0e81c709ef553c98cd435c79496dc52ef
Williams, Dave J (UG - Computer Science) <dw00506@surrey.ac.uk>
2020-08-14 16:05:30 +0200

    lesson 32

--- src/App.js
+++ src/App.js
@@ -6,15 +6,11 @@ import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
 import User from './components/users/User';
-import axios from 'axios';
-
 import GithubState from './context/github/GithubState';
 
 import './App.css';
 
 const App = () => {
-  const [repos, setRepos] = useState([]);
-  const [loading, setLoading] = useState(false);
   const [alert, setAlert] = useState(null);
 
   // async componentDidMount() {
@@ -25,17 +21,6 @@ const App = () => {
   //   );
   //   this.setState({ users: res.data, loading: false });
   // }
-
-  // Get users repos
-  const getUserRepos = async (username) => {
-    setLoading(true);
-
-    const res = await axios.get(
-      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-    );
-    setRepos(res.data);
-    setLoading(false);
-  };
 
   // Set alert
   const showAlert = (msg, type) => {
@@ -55,7 +40,7 @@ const App = () => {
               <Route
                 exact
                 path='/'
-                render={(props) => (
+                render={props => (
                   <Fragment>
                     <Search setAlert={showAlert} />
                     <Users />
@@ -63,13 +48,7 @@ const App = () => {
                 )}
               />
               <Route exact path='/about' component={About} />
-              <Route
-                exact
-                path='/user/:login'
-                render={(props) => (
-                  <User {...props} getUserRepos={getUserRepos} repos={repos} />
-                )}
-              />
+              <Route exact path='/user/:login' component={User} />
             </Switch>
           </div>
         </div>
